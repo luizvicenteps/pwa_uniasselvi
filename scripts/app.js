82cd72be28f1d867581e7f535d5078926be55092
Luiz Vicente Prestes <luizvicenteps@gmail.com>
2018-07-02 16:32:05 -0400

    Ajuste de Push Debug

--- scripts/app.js
+++ scripts/app.js
@@ -388,20 +388,13 @@
   }
 
   document.addEventListener('beforeinstallprompt', function(e) {
-    // beforeinstallprompt Event fired
-    console.log('Aqui é o prompt');
-    alert('Prompt de Instalação');
-    // e.userChoice will return a Promise.
-    // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
     e.userChoice.then(function(choiceResult) {
-  
       console.log(choiceResult.outcome);
-  
       if(choiceResult.outcome == 'dismissed') {
-        console.log('User cancelled home screen install');
+        console.log('Usuário Cancelou Instalação na Tela Home');
       }
       else {
-        console.log('User added to home screen');
+        console.log('Usuário adicionou na Tela Home');
       }
     });
   });
